0c83b763a745b44d2cd4a1db2f49d65d063d2420
zinedine <zhasni@student.42.fr>
2022-01-05 19:11:55 +0100

    Add test get Energy Account HTA [OK]

--- chaincode/src/controller/EnergyAccountController.ts
+++ chaincode/src/controller/EnergyAccountController.ts
@@ -1,4 +1,5 @@
 import { Context } from 'fabric-contract-api';
+import { date } from 'yup/lib/locale';
 import { OrganizationTypeMsp } from '../enums/OrganizationMspType';
 import { EnergyAccount } from '../model/energyAccount';
 
@@ -43,14 +44,46 @@ export class EnergyAccountController {
         );
     }
 
-    public static async getAllEnergyAccount(ctx: Context): Promise<string> {
+    public static async getEnergyAccount(
+            ctx: Context,
+            meteringPointMrid: string,
+            startCreatedDateTime: string): Promise<string> {
         const allResults = [];
-        const query = `{"selector": {"docType": "energyAccount"}}`;
-        const identity = await ctx.stub.getMspID();
-        if (identity !== OrganizationTypeMsp.RTE && identity !== OrganizationTypeMsp.ENEDIS) {
-            throw new Error(`Organisation, ${identity} does not have read access for Energy Account.`);
-        }
+        const dateUp = new Date(startCreatedDateTime);
+        console.log(meteringPointMrid);
+        console.log(startCreatedDateTime);
+        // console.log('new date=', new Date('2021-10-21T23:59:50.999Z'));
+        // console.log ('datesetmili=', dateUp.setUTCMilliseconds(0));
+        // console.log ('datesetsec=', dateUp.setUTCSeconds(0));
+        // console.log ('datesetmin=', dateUp.setUTCMinutes(0));
+        // console.log ('datesethour=', dateUp.setUTCHours(0));
+        // console.log ('datetmptime=', dateUp.getTime());
 
+        dateUp.setUTCMilliseconds(0);
+        dateUp.setUTCSeconds(0);
+        dateUp.setUTCMinutes(0);
+        dateUp.setUTCHours(0);
+        console.log('dateUp=', dateUp);
+        console.log('dateUp=', JSON.stringify(dateUp));
+        const dateDown = new Date(dateUp.getTime() + 86399999);
+        console.log('dateDown=', dateDown);
+        console.log('dateDown=', JSON.stringify(dateDown));
+
+        const query = `{
+            "selector":
+            {
+                "docType": "energyAccount",
+                "meteringPointMrid": "${meteringPointMrid}",
+                "deleteMeTime": "${startCreatedDateTime}",
+                "createdDateTime": {
+                    "$gte": ${JSON.stringify(dateUp)},
+                    "$lte": ${JSON.stringify(dateDown)}
+                },
+                "sort": [{
+                    "createdDateTime" : "desc"
+                }]
+            }
+        }`;
         const iterator = await ctx.stub.getQueryResult(query);
         let result = await iterator.next();
         while (!result.done) {
@@ -66,4 +99,28 @@ export class EnergyAccountController {
         }
         return JSON.stringify(allResults);
     }
+
+    // public static async getAllEnergyAccount(ctx: Context): Promise<string> {
+    //     const allResults = [];
+    //     const query = `{"selector": {"docType": "energyAccount"}}`;
+    //     const identity = await ctx.stub.getMspID();
+    //     if (identity !== OrganizationTypeMsp.RTE && identity !== OrganizationTypeMsp.ENEDIS) {
+    //         throw new Error(`Organisation, ${identity} does not have read access for Energy Account.`);
+    //     }
+
+    //     const iterator = await ctx.stub.getQueryResult(query);
+    //     let result = await iterator.next();
+    //     while (!result.done) {
+    //         const strValue = Buffer.from(result.value.value.toString()).toString('utf8');
+    //         let record;
+    //         try {
+    //             record = JSON.parse(strValue);
+    //         } catch (err) {
+    //             record = strValue;
+    //         }
+    //         allResults.push(record);
+    //         result = await iterator.next();
+    //     }
+    //     return JSON.stringify(allResults);
+    // }
 }

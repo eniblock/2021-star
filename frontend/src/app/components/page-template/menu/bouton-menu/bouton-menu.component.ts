d6fb1a7e21518431c4f660d5841284e318e7eeef
Jean-Rene Courtault <jrcourtault@theblockchainxdev.com>
2022-01-10 19:22:24 +0100

    STAR-176 template - step 2

---
+++ frontend/src/app/components/page-template/menu/bouton-menu/bouton-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bouton-menu',
+  templateUrl: './bouton-menu.component.html',
+  styleUrls: ['./bouton-menu.component.css']
+})
+export class BoutonMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- frontend/src/app/components/page-template/page-template.module.ts
+++ frontend/src/app/components/page-template/page-template.module.ts
@@ -4,10 +4,11 @@ import { PageTemplateComponent } from './page-template.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { MatDividerModule } from '@angular/material/divider';
 import { MenuComponent } from './menu/menu.component';
+import { BoutonMenuComponent } from './menu/bouton-menu/bouton-menu.component';
 
 @NgModule({
   imports: [CommonModule, AppRoutingModule, MatDividerModule],
-  declarations: [PageTemplateComponent, MenuComponent],
+  declarations: [PageTemplateComponent, MenuComponent, BoutonMenuComponent],
   exports: [PageTemplateComponent],
 })
 export class PageTemplateModule {}

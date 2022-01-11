d40d19b2f20201beb2f2e7752fed00f4270cb3d2
Jean-Rene Courtault <jrcourtault@theblockchainxdev.com>
2022-01-11 15:41:51 +0100

    STAR-176 template - step 4 - colors

--- frontend/src/app/pages/accueil/accueil.module.ts
+++ frontend/src/app/pages/accueil/accueil.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccueilRoutes } from './accueil.routing';
 import { AccueilComponent } from './accueil.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, AccueilRoutes],
+  imports: [CommonModule, AccueilRoutes, MatButtonModule],
   declarations: [AccueilComponent],
 })
 export class HomeModule {}

11fb962879495563d529fccfbf2710b5e184c804
Jean-Rene Courtault <jrcourtault@theblockchainxdev.com>
2022-01-20 18:41:54 +0100

    STAR-141 Pagination - step 1

---
+++ frontend/src/app/pipes/OrdreRechercheReseau.pipe.ts
@@ -0,0 +1,27 @@
+import { OrdreRechercheReseau } from 'src/app/models/enum/OrdreRechercheReseau.enum';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'OrdreRechercheReseau',
+})
+export class OrdreRechercheReseauPipe implements PipeTransform {
+  transform(
+    value: OrdreRechercheReseau | OrdreRechercheReseau[],
+    args?: any
+  ): any {
+    if (value instanceof Array) {
+      return value.map((t) => this.toString(t)).join(', ');
+    }
+    return this.toString(value);
+  }
+
+  toString(value: OrdreRechercheReseau): string {
+    switch (value) {
+      case OrdreRechercheReseau.producerMarketParticipantName:
+        return 'Lieu de production';
+      case OrdreRechercheReseau.technologyType:
+        return 'Filière';
+    }
+    return 'OrdreRechercheReseau inconnu';
+  }
+}

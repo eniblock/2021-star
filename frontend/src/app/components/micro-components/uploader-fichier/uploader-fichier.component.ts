70b39ff71ac5cf4e21a89921ca4547c49ad8e461
Jean-Rene Courtault <jrcourtault@theblockchainxdev.com>
2022-02-03 15:35:02 +0100

    STAR-232 import fichiers - step 3

--- frontend/src/app/components/micro-components/uploader-fichier/uploader-fichier.component.ts
+++ frontend/src/app/components/micro-components/uploader-fichier/uploader-fichier.component.ts
@@ -21,35 +21,15 @@ export class UploaderFichierComponent implements OnInit {
   ngOnInit() {}
 
   public dropped(files: NgxFileDropEntry[]) {
-    this.files = files;
     for (const droppedFile of files) {
-      // Is it a file?
       if (droppedFile.fileEntry.isFile) {
+        // On ajoute des fichiers
         const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
         fileEntry.file((file: File) => {
-          // Here you can access the real file
-          console.log(droppedFile.relativePath, file);
-
-          /**
-          // You could upload it like this:
-          const formData = new FormData()
-          formData.append('logo', file, relativePath)
-
-          // Headers
-          const headers = new HttpHeaders({
-            'security-token': 'mytoken'
-          })
-
-          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
-          .subscribe(data => {
-            // Sanitized logo returned from backend
-          })
-          **/
+          this.files.push(droppedFile);
         });
       } else {
-        // It was a directory (empty directories are added, otherwise only files)
-        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
-        console.log(droppedFile.relativePath, fileEntry);
+        // On ajoute un repertoire vide
       }
     }
   }

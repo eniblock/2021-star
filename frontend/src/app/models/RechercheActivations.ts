b31975cd166b2c1ff8ead562c172b7ce4221ed3c
Jean-Rene Courtault <jrcourtault@theblockchainxdev.com>
2022-02-24 15:12:23 +0100

    STAR-246 - resultat de la recherche - step 1

--- frontend/src/app/models/RechercheActivations.ts
+++ frontend/src/app/models/RechercheActivations.ts
@@ -1,4 +1,6 @@
 import { OrdreRechercheActivations } from './enum/OrdreRechercheActivations.enum';
+import { TechnologyType } from './enum/TechnologyType.enum';
+import { TypeLimitation } from './enum/TypeLimitation.enum';
 import { FormulairePagination } from './Pagination';
 
 export interface FormulaireRechercheActivations {
@@ -13,4 +15,22 @@ export interface RechercheActivationsRequete
   extends FormulairePagination<OrdreRechercheActivations>,
     FormulaireRechercheActivations {}
 
-export interface RechercheActivationsEntite {}
+export interface RechercheActivationsEntite {
+  technologyType?: TechnologyType;
+  originAutomationRegisteredResourceMrid?: string;
+  producerMarketParticipantName?: string;
+  siteName?: string;
+  producerMarketParticipantMrid?: string;
+  startCreatedDateTimeEnedis?: string; // JSON
+  endCreatedDateTimeEnedis?: string; // JSON
+  startCreatedDateTimeRte?: string; // JSON
+  endCreatedDateTimeRte?: string; // JSON
+  typeLimitation?: TypeLimitation;
+  quantity?: number;
+  motif?: {
+    messageType: string;
+    businessType: string;
+    reasonCode: string;
+  };
+  systemOperatorMarketParticipantRoleType: string; // Le type d'operator et donc de site (A49 => RTE, A50 => Enedis)
+}
--- frontend/src/app/services/api/activations.service.ts
+++ frontend/src/app/services/api/activations.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { OrdreRechercheActivations } from 'src/app/models/enum/OrdreRechercheActivations.enum';
+import { TechnologyType } from 'src/app/models/enum/TechnologyType.enum';
+import { TypeLimitation } from 'src/app/models/enum/TypeLimitation.enum';
 import {
   FormulairePagination,
   PaginationReponse,
@@ -26,6 +28,10 @@ export class ActivationsService {
     form: FormulaireRechercheActivations,
     pagination: FormulairePagination<OrdreRechercheActivations>
   ): Observable<PaginationReponse<RechercheActivationsEntite>> {
+    if (true) {
+      return this.getMocks(form, pagination);
+    }
+
     const formToSend: RechercheActivationsRequete = {
       ...form,
       ...pagination,
@@ -44,5 +50,139 @@ export class ActivationsService {
     let form = sessionStorage.getItem(this.CACHE_KEY);
     sessionStorage.removeItem(this.CACHE_KEY);
     return form == null ? null : JSON.parse(form);
+  }
+
+  /* *********************************************************
+                               MOCKS
+     ********************************************************* */
+
+  getMocks(
+    form: FormulaireRechercheActivations,
+    pagination: FormulairePagination<OrdreRechercheActivations>
+  ): Observable<PaginationReponse<RechercheActivationsEntite>> {
+    return of({
+      totalElements: 6,
+      bookmark: 'B_' + Math.random(),
+      content:
+        pagination.bookmark == null
+          ? [
+              {
+                technologyType: TechnologyType.EOLIEN,
+                originAutomationRegisteredResourceMrid: 'Poste 1',
+                producerMarketParticipantName: 'Boralex',
+                siteName: 'Ferme éolienne de Genonville',
+                producerMarketParticipantMrid: '17X000001307745X',
+                startCreatedDateTimeEnedis: '20220114T13:12:23.511Z',
+                endCreatedDateTimeEnedis: '20220114T15:12:23.511Z',
+                startCreatedDateTimeRte: '20220113T11:12:23.511Z',
+                endCreatedDateTimeRte: '20220114T13:14:23.511Z',
+                typeLimitation: TypeLimitation.MANUELLE,
+                quantity: 23,
+                motif: {
+                  messageType: 'A98',
+                  businessType: 'C55',
+                  reasonCode: 'A70',
+                },
+                systemOperatorMarketParticipantRoleType: 'A49',
+              },
+              {
+                technologyType: TechnologyType.EOLIEN,
+                originAutomationRegisteredResourceMrid: 'Poste 2',
+                producerMarketParticipantName: 'Boralex',
+                siteName: 'Ferme éolienne de Genonville',
+                producerMarketParticipantMrid: '17X000001307745X',
+                startCreatedDateTimeEnedis: '20220114T13:12:23.511Z',
+                endCreatedDateTimeEnedis: '20220114T15:12:23.511Z',
+                startCreatedDateTimeRte: '20220113T11:12:23.511Z',
+                endCreatedDateTimeRte: '20220114T13:14:23.511Z',
+                typeLimitation: TypeLimitation.MANUELLE,
+                quantity: 23,
+                motif: {
+                  messageType: 'A98',
+                  businessType: 'C55',
+                  reasonCode: 'A70',
+                },
+                systemOperatorMarketParticipantRoleType: 'A49',
+              },
+              {
+                technologyType: TechnologyType.EOLIEN,
+                originAutomationRegisteredResourceMrid: 'Poste 3',
+                producerMarketParticipantName: 'Boralex',
+                siteName: 'Ferme éolienne de Genonville',
+                producerMarketParticipantMrid: '17X000001307745X',
+                startCreatedDateTimeEnedis: '20220114T13:12:23.511Z',
+                endCreatedDateTimeEnedis: '20220114T15:12:23.511Z',
+                startCreatedDateTimeRte: '20220113T11:12:23.511Z',
+                endCreatedDateTimeRte: '20220114T13:14:23.511Z',
+                typeLimitation: TypeLimitation.MANUELLE,
+                quantity: 23,
+                motif: {
+                  messageType: 'A98',
+                  businessType: 'C55',
+                  reasonCode: 'A70',
+                },
+                systemOperatorMarketParticipantRoleType: 'A49',
+              },
+              {
+                technologyType: TechnologyType.EOLIEN,
+                originAutomationRegisteredResourceMrid: 'Poste 4',
+                producerMarketParticipantName: 'Boralex',
+                siteName: 'Ferme éolienne de Genonville',
+                producerMarketParticipantMrid: '17X000001307745X',
+                startCreatedDateTimeEnedis: '20220114T13:12:23.511Z',
+                endCreatedDateTimeEnedis: '20220114T15:12:23.511Z',
+                startCreatedDateTimeRte: '20220113T11:12:23.511Z',
+                endCreatedDateTimeRte: '20220114T13:14:23.511Z',
+                typeLimitation: TypeLimitation.MANUELLE,
+                quantity: 23,
+                motif: {
+                  messageType: 'A98',
+                  businessType: 'C55',
+                  reasonCode: 'A70',
+                },
+                systemOperatorMarketParticipantRoleType: 'A49',
+              },
+              {
+                technologyType: TechnologyType.EOLIEN,
+                originAutomationRegisteredResourceMrid: 'Poste 5',
+                producerMarketParticipantName: 'Boralex',
+                siteName: 'Ferme éolienne de Genonville',
+                producerMarketParticipantMrid: '17X000001307745X',
+                startCreatedDateTimeEnedis: '20220114T13:12:23.511Z',
+                endCreatedDateTimeEnedis: '20220114T15:12:23.511Z',
+                startCreatedDateTimeRte: '20220113T11:12:23.511Z',
+                endCreatedDateTimeRte: '20220114T13:14:23.511Z',
+                typeLimitation: TypeLimitation.MANUELLE,
+                quantity: 23,
+                motif: {
+                  messageType: 'A98',
+                  businessType: 'C55',
+                  reasonCode: 'A70',
+                },
+                systemOperatorMarketParticipantRoleType: 'A49',
+              },
+            ]
+          : [
+              {
+                technologyType: TechnologyType.PHOTOVOLTAIQUE,
+                originAutomationRegisteredResourceMrid: 'Poste 6',
+                producerMarketParticipantName: 'Boralex',
+                siteName: 'Ferme éolienne de Genonville',
+                producerMarketParticipantMrid: '17X000001307745X',
+                startCreatedDateTimeEnedis: '20220114T13:12:23.511Z',
+                endCreatedDateTimeEnedis: '20220114T15:12:23.511Z',
+                startCreatedDateTimeRte: '20220113T11:12:23.511Z',
+                endCreatedDateTimeRte: '20220114T13:14:23.511Z',
+                typeLimitation: TypeLimitation.MANUELLE,
+                quantity: 23,
+                motif: {
+                  messageType: 'A98',
+                  businessType: 'C55',
+                  reasonCode: 'A70',
+                },
+                systemOperatorMarketParticipantRoleType: 'A50',
+              },
+            ],
+    });
   }
 }

2817b9303bee71648dc246c204a4766502fec9f8
LEGBA Eric <eric.legba@gmail.com>
2022-03-31 13:03:06 +0200

    Fix erreur de connexion entre le backend et la blockchain

--- backend/api/src/main/java/com/star/rest/OrdreLimitationController.java
+++ backend/api/src/main/java/com/star/rest/OrdreLimitationController.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.star.enums.InstanceEnum.DSO;
 import static com.star.enums.InstanceEnum.PRODUCER;
 import static com.star.enums.InstanceEnum.TSO;
 import static org.apache.commons.collections4.CollectionUtils.isEmpty;

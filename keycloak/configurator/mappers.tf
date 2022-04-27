2739ec9a4b63584816a3a187a408d01fd05f8734
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2022-04-27 11:07:23 +0200

    switch to keycloak X
    also configure the users, smtp service and attribute mapper

---
+++ keycloak/configurator/mappers.tf
@@ -0,0 +1,15 @@
+resource "keycloak_generic_client_protocol_mapper" "producerMarketParticipantMrid" {
+  realm_id        = keycloak_realm.main.id
+  client_id       = keycloak_openid_client.frontend.id
+  name            = "producerMarketParticipantMrid"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usermodel-attribute-mapper"
+  config = {
+    "userinfo.token.claim" = "true"
+    "user.attribute"       = "producerMarketParticipantMrid"
+    "id.token.claim"       = "true"
+    "access.token.claim"   = "true"
+    "claim.name"           = "producerMarketParticipantMrid"
+    "jsonType.label"       = "String"
+  }
+}

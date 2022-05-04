ad2d4783a2c4515e3b47049c49046c2425bcfce1
Jean-René Courtault <jrcourtault@theblockchainxdev.com>
2022-05-04 08:56:57 +0000

    Star 392

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,49 +1,43 @@
-import { PageTemplateModule } from './components/page-template/page-template.module';
-import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { AppComponent } from './app.component';
-import { keycloakInitializer } from './factories/keycloak-initializer';
-import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServerErrorInterceptor } from './interceptors/serveur-error-interceptor';
+import {PageTemplateModule} from './components/page-template/page-template.module';
+import {APP_INITIALIZER, LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServerErrorInterceptor} from './interceptors/serveur-error-interceptor';
 import {
   DateAdapter,
   MatNativeDateModule,
   MAT_DATE_LOCALE,
 } from '@angular/material/core';
-import { CustomDateAdapter } from './adapters/custom-date-adapter';
+import {CustomDateAdapter} from './adapters/custom-date-adapter';
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {KeycloakInterceptor} from "./interceptors/keycloak-interceptor";
 
 @NgModule({
   imports: [
     AppRoutingModule,
     BrowserModule,
     HttpClientModule,
-    KeycloakAngularModule,
     BrowserAnimationsModule,
     MatSnackBarModule,
     PageTemplateModule,
     MatNativeDateModule,
+    MatProgressBarModule,
   ],
   declarations: [AppComponent],
   providers: [
     {
-      provide: APP_INITIALIZER,
-      useFactory: keycloakInitializer,
-      deps: [KeycloakService],
-      multi: true,
+      provide: HTTP_INTERCEPTORS, multi: true, useClass: ServerErrorInterceptor,
     },
-    {
-      provide: HTTP_INTERCEPTORS,
-      multi: true,
-      useClass: ServerErrorInterceptor,
-    },
-    { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' },
-    { provide: LOCALE_ID, useValue: 'fr' },
-    { provide: DateAdapter, useClass: CustomDateAdapter },
+    {provide: HTTP_INTERCEPTORS, multi: true, useClass: KeycloakInterceptor},
+    {provide: MAT_DATE_LOCALE, useValue: 'fr-FR'},
+    {provide: LOCALE_ID, useValue: 'fr'},
+    {provide: DateAdapter, useClass: CustomDateAdapter},
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ frontend/src/app/services/common/keycloak.service.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import Keycloak, {KeycloakConfig} from "keycloak-js";
+import {environment} from "../../../environments/environment";
+import {from, Observable, of} from "rxjs";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KeycloakService {
+
+  keycloak: Keycloak;
+
+  constructor() {
+    const config: KeycloakConfig = {
+      url: environment.keycloak.issuer
+        || window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '') + '/auth/',
+      realm: environment.keycloak.realm,
+      clientId: environment.keycloak.clientId,
+    };
+    this.keycloak = new Keycloak(config);
+  }
+
+  init(): Observable<boolean> {
+    return from(this.keycloak.init({onLoad: 'login-required'}));
+  }
+
+  logout(): Observable<void> {
+    return from(this.keycloak.logout());
+  }
+
+  getToken(fresh: boolean): Observable<string | null> {
+    if (fresh) {
+      return from(this.keycloak.updateToken(10))
+        .pipe(
+          map(hasBeenRefreshed => this.keycloak.token || null)
+        )
+    } else {
+      return of(this.keycloak.token || null);
+    }
+  }
+
+}

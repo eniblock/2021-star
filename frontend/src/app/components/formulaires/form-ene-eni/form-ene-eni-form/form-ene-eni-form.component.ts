e22d367a1b1b4d4f1520a1165b90580b3924ecba
Jean-Rene Courtault <jrcourtault@theblockchainxdev.com>
2022-05-13 14:39:27 +0200

    STAR-330 sauv

--- frontend/src/app/components/formulaires/form-ene-eni/form-ene-eni-form/form-ene-eni-form.component.ts
+++ frontend/src/app/components/formulaires/form-ene-eni/form-ene-eni-form/form-ene-eni-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {OrdreLimitationService} from "../../../../services/api/ordre-limitation.service";
+import {EnergyAmountService} from "../../../../services/api/energy-amount.service";
+import {MatStepper} from "@angular/material/stepper";
+import {DateHelper} from "../../../../helpers/date.helper";
 
 @Component({
   selector: 'app-form-ene-eni-form',
@@ -6,10 +11,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-ene-eni-form.component.css']
 })
 export class FormEneEniFormComponent implements OnInit {
+  form: FormGroup = this.formBuilder.group({
+    activationDocumentMrid: ['', Validators.required],
+    revisionNumber: ['', Validators.required],
+    processType: ['A42', Validators.required],
+    businessType: ['C55', Validators.required],
+    classificationType: ['', Validators.required],
+    quantity: ['', Validators.required],
+    measurementUnitName: ['', Validators.required],
+    timestampDateStart: ['', Validators.required],
+    timestampTimeStart: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('[0-2]?[0-9]:[0-5]?[0-9]:[0-5]?[0-9]'),
+      ],
+    ],
+    timestampDateEnd: ['', Validators.required],
+    timestampTimeEnd: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('[0-2]?[0-9]:[0-5]?[0-9]:[0-5]?[0-9]'),
+      ],
+    ],
+  });
 
-  constructor() { }
+  startDatetimeLimitation: Date = new Date();
+  endDatetimeLimitation: Date = new Date();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private energyAmountService: EnergyAmountService,
+  ) {
+  }
 
   ngOnInit(): void {
+  }
+
+  toResume(stepperRef: MatStepper) {
+    /*
+    this.endCreatedDateTime = DateHelper.toDatetime(
+      this.form.get('timestampDate')?.value,
+      this.form.get('timestampTime')?.value
+    );
+     */
+    stepperRef.next();
+  }
+
+  onSubmit(stepperRef: MatStepper) {
+    /*
+    const form = {
+      ...this.form.value,
+      endCreatedDateTime: this.endCreatedDateTime.toJSON().split('.')[0] + 'Z',
+      messageType: this.form.value.messageType.code,
+      businessType: this.form.value.businessType.code,
+      reasonCode: this.form.value.reasonCode.code,
+    };
+
+    delete form.timestampDate;
+    delete form.timestampTime;
+
+    this.ordreLimitationService.creerOrdreFin(form).subscribe((ok) => {
+      stepperRef.next(); // On passe sur le dernier step si ca s'est bien passé
+    });
+     */
+  }
+
+  reset(stepperRef: MatStepper) {
+    stepperRef.reset();
   }
 
 }

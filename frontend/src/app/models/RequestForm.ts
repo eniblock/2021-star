3825ecd96c00b337b57194599454085fcc645e87
Jean-Rene Courtault <jrcourtault@theblockchainxdev.com>
2022-06-22 14:39:06 +0200

    STAR-463 SAUV

--- frontend/src/app/models/RequestForm.ts
+++ frontend/src/app/models/RequestForm.ts
@@ -5,19 +5,3 @@ export interface RequestForm<E> {
   order: E; // Le champs sur lequel on trie
   orderDirection: OrderDirection; // La direction du tri ("asc" ou "desc")
 }
-
-/* TODO : delete !!! */
-export interface FormulairePagination<E> {
-  // E => enum indiquant le nom du champ sur lequel on trie
-  pageSize: number;
-  bookmark: string | null; // Bookmark de la precedente requete dans le cas où on veut la page suivante (null si on veut la première page)
-  order: E; // Le champs sur lequel on trie
-  orderDirection: OrderDirection; // La direction du tri ("asc" ou "desc")
-}
-
-/* TODO : delete !!! */
-export interface PaginationReponse<E> {
-  totalElements: number; // Le nombre total d'éléments
-  bookmark?: string; // Bookmark de la requète envoyée à la blockchain (utile pour la pagination)
-  content: E[]; // Les resultats
-}

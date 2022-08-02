6e928b7d4b9ddc912aa9ecd4066556cf492b09a3
Ali Hassan AYOUJIL <ay.alihassan@gmail.com>
2022-08-02 15:14:34 +0000

    fix keycloak configurator bug

--- keycloak/configurator/mappers.tf
+++ keycloak/configurator/mappers.tf
@@ -14,18 +14,3 @@ resource "keycloak_generic_client_protocol_mapper" "producerMarketParticipantMri
   }
 }
 
-resource "keycloak_generic_client_protocol_mapper" "systemOperatorMarketParticipantMrid" {
-  realm_id        = keycloak_realm.main.id
-  client_id       = keycloak_openid_client.frontend.id
-  name            = "systemOperatorMarketParticipantMrid"
-  protocol        = "openid-connect"
-  protocol_mapper = "oidc-usermodel-attribute-mapper"
-  config = {
-    "userinfo.token.claim" = "true"
-    "user.attribute"       = "systemOperatorMarketParticipantMrid"
-    "id.token.claim"       = "true"
-    "access.token.claim"   = "true"
-    "claim.name"           = "systemOperatorMarketParticipantMrid"
-    "jsonType.label"       = "String"
-  }
-}

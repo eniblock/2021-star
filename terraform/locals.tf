a21349b55ab2d8bdcc6e2e26fb47e485048aa1c8
Gaëtan Lehmann <glehmann@theblockchainxdev.com>
2022-08-24 11:15:10 +0200

    don't use .gra. in the peer domain name

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -7,40 +7,32 @@ locals {
       sbg5 = {
         monitoring_domain      = "obs.${local.env}.star.eniblock.fr"
         project_fqdn_testing   = [
-            "*.testing.star.eniblock.fr",
-            "orderer1.orderer.testing.star.eniblock.fr", 
-            "orderer2.orderer.testing.star.eniblock.fr", 
+            "orderer1.orderer.testing.star.eniblock.fr",
+            "orderer2.orderer.testing.star.eniblock.fr",
             "orderer3.orderer.testing.star.eniblock.fr",
-            "peer1.enedis.testing.star.eniblock.fr", 
-            "peer1.producer.testing.star.eniblock.fr", 
+            "peer1.enedis.testing.star.eniblock.fr",
+            "peer1.producer.testing.star.eniblock.fr",
             "peer1.rte.testing.star.eniblock.fr",
-            "peer2.enedis.testing.star.eniblock.fr", 
-            "peer2.producer.testing.star.eniblock.fr", 
-            "peer2.rte.testing.star.eniblock.fr"
         ]
         project_fqdn_staging   = [
-            "*.staging.star.eniblock.fr",
-            "orderer1.orderer.staging.star.eniblock.fr", 
-            "orderer2.orderer.staging.star.eniblock.fr", 
+            "orderer1.orderer.staging.star.eniblock.fr",
+            "orderer2.orderer.staging.star.eniblock.fr",
             "orderer3.orderer.staging.star.eniblock.fr",
-            "peer1.enedis.staging.star.eniblock.fr", 
-            "peer1.producer.staging.star.eniblock.fr", 
+            "peer1.enedis.staging.star.eniblock.fr",
+            "peer1.producer.staging.star.eniblock.fr",
             "peer1.rte.staging.star.eniblock.fr",
-            "peer2.enedis.staging.star.eniblock.fr", 
-            "peer2.producer.staging.star.eniblock.fr", 
-            "peer2.rte.staging.star.eniblock.fr"
         ]
         project_fqdn_prod      = [
             "enedis.star.eniblock.fr",
             "producer.star.eniblock.fr",
-            "orderer1.orderer.prod.star.eniblock.fr", 
-            "orderer2.orderer.prod.star.eniblock.fr", 
+            "orderer1.orderer.prod.star.eniblock.fr",
+            "orderer2.orderer.prod.star.eniblock.fr",
             "orderer3.orderer.prod.star.eniblock.fr",
-            "peer1.enedis.prod.star.eniblock.fr", 
-            "peer1.producer.prod.star.eniblock.fr", 
+            "peer1.enedis.prod.star.eniblock.fr",
+            "peer1.producer.prod.star.eniblock.fr",
             "peer1.rte.prod.star.eniblock.fr",
-            "peer2.enedis.prod.star.eniblock.fr", 
-            "peer2.producer.prod.star.eniblock.fr", 
+            "peer2.enedis.prod.star.eniblock.fr",
+            "peer2.producer.prod.star.eniblock.fr",
             "peer2.rte.prod.star.eniblock.fr"
         ]
         s3_container_region    = "sbg"
@@ -50,17 +42,14 @@ locals {
       gra7 = {
         monitoring_domain      = "obs.${local.env}.star.eniblock.fr"
          project_fqdn_testing = [
-            "peer1.enedis.testing.gra.star.eniblock.fr", 
-            "peer1.producer.testing.gra.star.eniblock.fr", 
-            "peer1.rte.testing.gra.star.eniblock.fr",
-            "peer2.enedis.testing.gra.star.eniblock.fr", 
-            "peer2.producer.testing.gra.star.eniblock.fr", 
-            "peer2.rte.testing.gra.star.eniblock.fr"
+            "peer2.enedis.testing.star.eniblock.fr",
+            "peer2.producer.testing.star.eniblock.fr",
+            "peer2.rte.testing.star.eniblock.fr"
         ]
          project_fqdn_staging = [
-        #    "peer2.enedis.staging.star.eniblock.fr", 
-        #    "peer2.producer.staging.star.eniblock.fr", 
-        #    "peer2.rte.staging.star.eniblock.fr"
+            "peer2.enedis.staging.star.eniblock.fr",
+            "peer2.producer.staging.star.eniblock.fr",
+            "peer2.rte.staging.star.eniblock.fr"
         ]
         project_fqdn_prod      = [
         ]

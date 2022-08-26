41647fa07a9fee547e2f5dfdf81ef2c0aa3f89a4
Hubert Marteau <hmarteau@theblockchainxdev.com>
2022-08-26 10:46:24 +0200

    deletion RTE Activation Documents with potentialParent false

--- chaincode/src/controller/SiteController.ts
+++ chaincode/src/controller/SiteController.ts
@@ -11,6 +11,8 @@ import { SystemOperatorService } from './service/SystemOperatorService';
 import { ProducerService } from './service/ProducerService';
 import { Producer } from '../model/producer';
 import { DataReference } from '../model/dataReference';
+import { ActivationDocumentService } from './service/ActivationDocumentService';
+import { HLFServices } from './service/HLFservice';
 
 export class SiteController {
     public static async createSite(
@@ -229,9 +231,52 @@ export class SiteController {
         // const iterator = await SiteService.getQueryResult(ctx, query);
         // let results = await this.getAllResults(iterator);
 
-        let results = await SiteService.getQueryArrayResult(ctx, params, query);
+        const collections: string[] = await HLFServices.getCollectionsFromParameters(params, ParametersType.DATA_TARGET, ParametersType.ALL);
 
-        return results;
+        console.info("############################################")
+        for (var collection of collections) {
+            console.info("--------------")
+            console.info(collection)
+            console.info("--------------")
+            console.info("delete 0754217c-f0e5-4c23-823d-0533e391245b")
+            await ActivationDocumentService.delete(ctx, params, "0754217c-f0e5-4c23-823d-0533e391245b", collection);
+            console.info("END delete 0754217c-f0e5-4c23-823d-0533e391245b")
+            await ActivationDocumentService.delete(ctx, params, "0d818433-6095-4acf-b032-e4b6b8550c93", collection);
+            await ActivationDocumentService.delete(ctx, params, "16e6fb68-3c50-4ccc-8d97-2344eebcefe4", collection);
+            await ActivationDocumentService.delete(ctx, params, "1d454690-4a51-4086-8314-d05a15aa544a", collection);
+            await ActivationDocumentService.delete(ctx, params, "3b29a075-01e4-4786-88ab-5fd1c9830149", collection);
+            await ActivationDocumentService.delete(ctx, params, "4c845adb-4ce7-4803-92e5-32d54535f5a8", collection);
+            await ActivationDocumentService.delete(ctx, params, "5939d00e-9573-4323-80c9-880890c5212f", collection);
+            await ActivationDocumentService.delete(ctx, params, "6162e519-4984-48fc-941e-00c3b617b84e", collection);
+            await ActivationDocumentService.delete(ctx, params, "6835717f-87bd-41c9-9401-d259b61149a4", collection);
+            await ActivationDocumentService.delete(ctx, params, "77478df5-13df-43be-bc12-53077b7c92da", collection);
+            await ActivationDocumentService.delete(ctx, params, "9042c66c-b748-4e77-bf18-3a53fdf5a484", collection);
+            await ActivationDocumentService.delete(ctx, params, "9175a598-2dfe-4079-b37c-84572bf27709", collection);
+            await ActivationDocumentService.delete(ctx, params, "9ed75fb2-4430-488c-a772-ef036833cab0", collection);
+            await ActivationDocumentService.delete(ctx, params, "a66eacd0-91fa-4b97-b5c3-d59fe1268a24", collection);
+            await ActivationDocumentService.delete(ctx, params, "b50056fa-3ca3-4a36-8a3d-2380f4084316", collection);
+            await ActivationDocumentService.delete(ctx, params, "c19b0bdf-39ce-4284-8965-c995aec00857", collection);
+            await ActivationDocumentService.delete(ctx, params, "c7613779-70b5-43c7-ba6c-66c3f5b4a1a8", collection);
+            await ActivationDocumentService.delete(ctx, params, "cdd32d1a-3ce4-4ce1-83f1-355eb674cca2", collection);
+            await ActivationDocumentService.delete(ctx, params, "d0c4d05c-3e2c-4f7c-acc0-fa740f156822", collection);
+            await ActivationDocumentService.delete(ctx, params, "d5af2077-5b75-423c-95bd-bd0795918aa6", collection);
+            await ActivationDocumentService.delete(ctx, params, "d635223f-0a16-4645-a6a0-2b3da7863ff2", collection);
+            await ActivationDocumentService.delete(ctx, params, "ea7edef5-cf12-4234-9972-45b1a18e4668", collection);
+            await ActivationDocumentService.delete(ctx, params, "f11ec379-dc02-49b1-9c77-394b6eca1dc3", collection);
+            await ActivationDocumentService.delete(ctx, params, "f1253bf4-bca8-43bc-9147-51bcc3c458a9", collection);
+            await ActivationDocumentService.delete(ctx, params, "f1a14684-23e5-4529-93a5-35909a3e84fd", collection);
+            await ActivationDocumentService.delete(ctx, params, "fdbfc324-1756-4165-a03d-2a9c421ee1c2", collection);
+            await ActivationDocumentService.delete(ctx, params, "205033fe-0004-46e9-838e-d71fb54439fc", collection);
+            await ActivationDocumentService.delete(ctx, params, "4958b8c2-857f-4ba2-949c-8e79f8533adc", collection);
+            await ActivationDocumentService.delete(ctx, params, "c4dd6569-5e3b-4e3d-b47a-f6880d6d72d9", collection);
+        }
+        console.info("############################################")
+
+
+        // let results = await SiteService.getQueryArrayResult(ctx, params, query);
+
+        // return results;
+        return "";
     }
 
 

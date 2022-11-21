769a78516cb45a848bb6143802a6197c478b8994
Jean-Rene Courtault <jrcourtault@theblockchainxdev.com>
2022-11-21 13:44:45 +0100

    typo

--- chaincode/src/enums/IndemnityStatus.ts
+++ chaincode/src/enums/IndemnityStatus.ts
@@ -2,6 +2,6 @@ export enum IndeminityStatus {
     IN_PROGRESS = 'InProgress',
     AGREEMENT = 'Agreement',
     PROCESSED = 'Processed',
-    WAITING_INVOICE = 'WaintingInvoice',
+    WAITING_INVOICE = 'WaitingInvoice',
     INVOICE_SENT = 'InvoiceSent',
 }
